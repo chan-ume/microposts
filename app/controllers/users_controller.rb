bc7da6d5f2bd679abcb16f20759ae1684c6f47fa
chan_ume <dottobasic673@gmail.com>
2015-08-04 00:32:10 +0000

    almost complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts
+    @following_users = @user.following_users
   end
 
   def new
